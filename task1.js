2854124bbf17d58ee17d2209d8f486266d4e7cb9
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-03-18 17:59:12 +0530

    editing HTML using DOM

--- task1.js
+++ task1.js
@@ -63,32 +63,114 @@ console.log (newitem);*/
 
 // query selectors
 
-var itemlist = document.querySelectorAll('.list-group-item');
-var li = document.querySelector('li');
+// var itemlist = document.querySelectorAll('.list-group-item');
+// var li = document.querySelector('li');
 
 
-var seconditemlist = document.querySelector('.list-group-item:nth-child(2)');
-seconditemlist.style.backgroundColor = 'green';
+// var seconditemlist = document.querySelector('.list-group-item:nth-child(2)');
+// seconditemlist.style.backgroundColor = 'green';
 
 
-var thirditemlist = document.querySelector('.list-group-item:nth-child(3)');
-thirditemlist.style.display = 'none';
-console.log("queryselector");
-console.log(li);
+// var thirditemlist = document.querySelector('.list-group-item:nth-child(3)');
+// thirditemlist.style.display = 'none';
+// console.log("queryselector");
+// console.log(li);
 
 
 
 
-// querySelectorall
+// // querySelectorall
 
-var nodeelement = document.querySelectorAll('.list-group-item');
-console.log("querySelectorAll");
-console.log(nodeelement);
-nodeelement[1].style.color='green';
+// var nodeelement = document.querySelectorAll('.list-group-item');
+// console.log("querySelectorAll");
+// console.log(nodeelement);
+// nodeelement[1].style.color='green';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
  
- for(var i = 0 ; i < odd.length; i++)
- {
-    odd[i].style.backgroundColor ='green';
- }
+//  for(var i = 0 ; i < odd.length; i++)
+//  {
+//     odd[i].style.backgroundColor ='green';
+//  }
+
+
+
+// traversing DOM
+
+// parent node
+// var order = document.querySelector('#items');
+
+// order.parentNode.style.backgroundColor = "#f4f4f4"; 
+// console.log(order.parentNode.parentNode);
+// console.log(order.parentNode.parentNode.parentNode);
+
+ 
+
+//parent Element
+
+ var order = document.querySelector('#items');
+ //console.log(order.parentNode);
+// console.log(order.parentElement);
+// order.parentElement.style.backgroundColor = "#f4f4f4"; 
+// console.log(order.parentElement.parentElement);
+// console.log(order.parentElement.parentElement.parentElement);
+
+// Child Nodes - gives array of nodes with text which is white spaces.
+
+console.log(order.childNodes);
+//children  - gives the Html collection without any spaces.
+console.log(order.children);
+
+
+//firstchild
+console.log(order.firstChild);
+// It will gives us text that is white spaces.
+console.log("FirstELEMENTchild",order.firstElementChild);
+order.firstElementChild.textContent = "HEllo Item 1";
+
+
+
+// last child
+console.log(order.lastChild);
+console.log(order.lastElementChild);
+
+// nextsibling
+console.log(order.nextSibling);
+console.log(order.nextElementSibling);
+
+// previous Sibling
+console.log(order.previousSibling);
+console.log(order.previousElementSibling);
+order.previousElementSibling.style.color ='green';
+
+// create element
+
+// create a div 
+var newDiv = document.createElement('div');
+
+// add a class
+newDiv.className ='hello';
+
+
+// add an id
+newDiv.id = 'hello1';
+
+// set attribute
+newDiv.setAttribute('title','Hello Div');
+
+
+// create  text node
+var newDivText = document.createTextNode("Hello World");
+
+// adding text to div
+newDiv.appendChild(newDivText);
+
+// inserting text to html 
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+container.insertBefore(newDiv, h1);
+
+
