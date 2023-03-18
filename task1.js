ef1b68819afc0c6f449df24ed4b762c7efe2fd95
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-03-18 16:46:02 +0530

    queryselector's

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 // examine the dom
 
 
-console.dir(document);
+/*console.dir(document);
 console.log(document.location);
 console.log(document.URL);
 console.log(document.title);
@@ -11,15 +11,15 @@ console.log(document.doctype);
 console.log(document.forms);
 console.log("DOCUMENT ALL");
 console.log(document.all);
-console.log(document.all[10]);
+console.log(document.all[10]);*/
 
 
 
 // get element by id
 
-var title = document.getElementById("header-title");
-var header = document.getElementById("main-header");
-console.log(title);
+//var title = document.getElementById("header-title");
+//var header = document.getElementById("main-header");
+//console.log(title);
 //console.log(typeof title);
 // innerText  does  follow script or style  - Inner text
 //console.log(title.innerText);
@@ -33,31 +33,62 @@ console.log(title);
 
 
 // get element by class name
-var items = document.getElementsByClassName("list-group-item");
-console.log("elementsbyclassname");
-console.log(items);
+//var items = document.getElementsByClassName("list-group-item");
+//console.log("elementsbyclassname");
+//console.log(items);
 //console.log(items[1]);
-items[1].textContent = " Hello 2";
+//items[1].textContent = " Hello 2";
 /*items[0].style.fontWeight = 'bold';
 items[1].style.fontWeight = 'bold';
 items[2].style.fontWeight = 'bold';
 items[2].style.background = 'green';
 items[3].style.fontWeight = 'bold';*/
 
-for(var i = 0; i < items.length;i++)
+/*for(var i = 0; i < items.length;i++)
 {
     items[i].style.backgroundColor = "grey";
     items[i].style.fontWeight = 'bold';
 }
 
-items[2].style.backgroundColor = 'green';
+items[2].style.backgroundColor = 'green';*/
 
 // get element by tag name
 
- var newitem = document.getElementsByTagName('li');
+ /*var newitem = document.getElementsByTagName('li');
  console.log("elementbytagname");
  console.log(newitem);
  newitem[4].style.backgroundColor = "blue"; 
  newitem[5].style.textDecorationLine ='none';
-console.log (newitem);
+console.log (newitem);*/
 
+// query selectors
+
+var itemlist = document.querySelectorAll('.list-group-item');
+var li = document.querySelector('li');
+
+
+var seconditemlist = document.querySelector('.list-group-item:nth-child(2)');
+seconditemlist.style.backgroundColor = 'green';
+
+
+var thirditemlist = document.querySelector('.list-group-item:nth-child(3)');
+thirditemlist.style.display = 'none';
+console.log("queryselector");
+console.log(li);
+
+
+
+
+// querySelectorall
+
+var nodeelement = document.querySelectorAll('.list-group-item');
+console.log("querySelectorAll");
+console.log(nodeelement);
+nodeelement[1].style.color='green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+ 
+ for(var i = 0 ; i < odd.length; i++)
+ {
+    odd[i].style.backgroundColor ='green';
+ }
