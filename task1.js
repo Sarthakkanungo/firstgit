a07c0813e17eb92f29d1158452c33e466374a641
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-03-18 14:14:52 +0530

    changes through loop

--- task1.js
+++ task1.js
@@ -37,12 +37,19 @@ var items = document.getElementsByClassName("list-group-item");
 console.log(items);
 //console.log(items[1]);
 items[1].textContent = " Hello 2";
-items[0].style.fontWeight = 'bold';
+/*items[0].style.fontWeight = 'bold';
 items[1].style.fontWeight = 'bold';
 items[2].style.fontWeight = 'bold';
 items[2].style.background = 'green';
-items[3].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';*/
 
+for(var i = 0; i < items.length;i++)
+{
+    items[i].style.backgroundColor = "grey";
+    items[i].style.fontWeight = 'bold';
+}
+
+items[2].style.backgroundColor = 'green';
 
 
 
