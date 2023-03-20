73a824f2e86fe528dc109c828a580929f23cae37
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-03-20 12:43:15 +0530

    Appointment call with local storage

--- register.js
+++ register.js
@@ -13,15 +13,23 @@ function saveToLocalStorage(event)
     event.preventDefault();
         const name = event.target.username.value;
         const email = event.target.emailid.value;
+        const phone = event.target.Phoneno.value;
+        const Address = event.target.Address.value;
+        const date = event.target.date.value;
+        const time = event.target.time.value;
         //creating obj to store all values in an object
 
     const obj = { 
         name : name ,
-        email : email 
+        email : email,
+        phone : phone,
+       Address : Address,
+       date: date,
+       time: time
        }
    // converting object to its real values using JSON.stringify method ,,, and reverse it back as old object 
    // by using JSON.parse(obj)  
-    localStorage.setItem('obj' , JSON.stringify(obj));
+    localStorage.setItem(email , JSON.stringify(obj));
     
    
  }
@@ -34,8 +42,14 @@ function saveToLocalStorage(event)
 // for adding new li node as values put by user.
 
 
-/*const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+ const nameInput = document.querySelector('#name');
+ const emailInput = document.querySelector('#email');
+ const phoneInput = document.querySelector('#phone');
+ const AddressInput = document.querySelector('#add');
+ const DateInput = document.querySelector('#Date');
+ const TimeInput = document.querySelector('#Time1');
+
+
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
@@ -45,7 +59,7 @@ function onSubmit(e) {
   e.preventDefault();
   
   if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
+   
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
 
@@ -57,21 +71,23 @@ function onSubmit(e) {
     
 
     // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value} : ${phoneInput.value}: ${AddressInput.value}: ${DateInput.value} : ${TimeInput.value} ` ));
    
    
    
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
+    
     // Append to ul
     userList.appendChild(li);
 
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
+    phoneInput.value = '';
+    AddressInput.value = '';
+    TimeInput.value = '';
+    DateInput.value = '';
   }
-}*/
+}
 
 
  
