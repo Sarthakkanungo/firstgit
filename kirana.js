e89c078d115b29e240adfc2a20ee0675eba94900
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-05-19 18:17:40 +0530

    updated with total price

--- kirana.js
+++ kirana.js
@@ -1,7 +1,8 @@
-const url ="https://crudcrud.com/api/602873fdb3c24bcbbb0e76bd95f0cf28";
+const url = "https://crudcrud.com/api/9ac823f6f1da47619015b7eac7bb9cec";
 
-function InventoryList(event)
-{
+let total = 0;
+
+async function InventoryList(event) {
     event.preventDefault();
     const product = event.target.Product.value;
     const price = event.target.Price.value;
@@ -14,88 +15,115 @@ function InventoryList(event)
         quantity
     }
 
-    axios.post("https://crudcrud.com/api/602873fdb3c24bcbbb0e76bd95f0cf28/Kirana", obj)
-    .then((response) =>
-    {
-        console.log(response);
-        showItems(response.data);
-    })
-    .catch((error)=>
-    {
-        document.body.innerHTML = document.body.innerHTML + "<h4>Some Error Happened</h4>";
-        console.log(error);
-    })
-    
-}
+    try {
+        const response = await axios.post(`${url}/Kirana`, obj);
 
-window.addEventListener("DOMContentLoaded", ()=>
-{
-    axios.get("https://crudcrud.com/api/602873fdb3c24bcbbb0e76bd95f0cf28/Kirana")
-    .then((response) =>
-    {
-        console.log(response);
-        for(var i = 0 ; i <response.data.length ; i++)
-        {
-            showItems(response.data[i]);
-        }
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-});
-
-function showItems(item)
-{
-    
-     
-        document.getElementById('product').value = '';
-        document.getElementById('price').value = '';
-        document.getElementById('quantity').value = '';
-
-        const parentNode1 = document.getElementById('ItemList');
-        const child = 
-        `<li id=${item._id}> ${item.product} - ${item.price} Rs - <span>${item.quantity}<span>
-        <button onclick=buyOne('${item._id}','${item.quantity}')>Buy 1</button> </li>` 
-        // <button onclick=buyTwo('${item._id}',${item.Quantity},'${item.Price}')>Buy 2</button>
-        // <button onclick=buyThree('${item._id}',${item.Quantity},'${item.Price}')>Buy 3</button>
-             
-
-        parentNode1.innerHTML = parentNode1.innerHTML + child;
-
-}
-
-
-function showUpdatedItems(itemid,quantity)
-{
-    axios.get(`${url}/Kirana/${itemid}`)
-    .then((response) => {
         console.log(response.data);
-        
-        response.data.quantity=quantity;
-        
-        showItems(response.data);
+        showItemsonScreen(response.data);
 
-    })
-    .catch((err) => {
+    } catch (err) {
+        document.body.innerHTML = document.body.innerHTML + "<h4>Some Error Happened</h4>";
+        console.log(err);
+    }
+
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const getdetail = await axios.get(`${url}/Kirana`)
+
+        console.log(getdetail);
+        for (var i = 0; i < getdetail.data.length; i++) {
+            showItemsonScreen(getdetail.data[i]);
+        }
+    } catch (err) {
+        document.body.innerHTML = document.body.innerHTML + "<h4>Some Error Happened</h4>";
+        console.log(err);
+    }
+
+})
+
+function showItemsonScreen(item) {
+
+
+    document.getElementById('product').value = '';
+    document.getElementById('price').value = '';
+    document.getElementById('quantity').value = '';
+
+    const parentNode1 = document.getElementById('ItemList');
+    const child =
+        `<li id=${item._id}> ${item.product} - ${item.price} Rs - <span>${item.quantity}<span>
+            <button onclick=buyOne('${item._id}','${item.quantity}',${item.price})>Buy 1</button> 
+            <button onclick=buyTwo('${item._id}','${item.quantity}',${item.price})>Buy 2</button>
+            <button onclick=buyThree('${item._id}','${item.quantity}',${item.price})>Buy 3</button>
+            </li>`
+
+    parentNode1.innerHTML = parentNode1.innerHTML + child;
+
+}
+
+
+async function showUpdatedItems(itemid, quantity) {
+    try {
+        const show = await axios.get(`${url}/Kirana/${itemid}`)
+
+        console.log(show.data);
+
+        show.data.quantity = quantity;
+
+        showItemsonScreen(show.data);
+
+    } catch (err) {
         console.log(err)
-    })
+    }
+
+
+}
+
+function buyOne(itemid, quantity, price) {
+    if (quantity < 1) {
+        alert("Item not available")
+    }
+    quantity -= 1;
+    
+    const element = document.getElementById(itemid);
+    const parent = document.getElementById('ItemList');
+    parent.removeChild(element);
+    showUpdatedItems(itemid, quantity);
+    totalPrice(1*price);
+}
+
+function buyTwo(itemid, quantity, price) {
+    if (quantity < 2) {
+        alert("Item not available")
+    }
+    quantity -= 2;
+    
+    const element = document.getElementById(itemid);
+    const parent = document.getElementById('ItemList');
+    parent.removeChild(element);
+    showUpdatedItems(itemid, quantity);
+    totalPrice(2*price);
+}
+
+function buyThree(itemid, quantity, price) {
+    if (quantity < 3) {
+        alert("Item not available")
+    }
+    quantity -= 3;
+    
+    const element = document.getElementById(itemid);
+    const parent = document.getElementById('ItemList');
+    parent.removeChild(element);
+    showUpdatedItems(itemid, quantity);
+    totalPrice(3*price);
 }
 
 
-function buyOne(itemid,quantity)
-{
-   if(quantity < 1)
-   {
-     alert("Item not available")
-   }
-  quantity -= 1;
-   const element = document.getElementById(itemid);
-   const parent  = document.getElementById('ItemList');
-   parent.removeChild(element);
-   showUpdatedItems(itemid,quantity);
+function totalPrice(cost)
+{  
+    total += cost;
+    const totalCostElement = document.getElementById('Totalcost');
+    totalCostElement.textContent = total;
 }
- function totalprice(itemid,quantity,price)
- {
-
- }
 
